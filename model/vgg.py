44b040ffa8dcf949b2a13b691a7a56e0ef2406cf
fanghenshao <fanghenshao@sjtu.edu.cn>
2020-10-28 16:59:23 +0800

    vanilla network definition

---
+++ model/vgg.py
@@ -0,0 +1,67 @@
+"""
+Created on Mon Mar 23 2020
+
+@author: fanghenshao
+"""
+
+import torch
+import torch.nn as nn
+
+cfg = {
+    'A' : [64,     'M', 128,      'M', 256, 256,           'M', 512, 512,           'M', 512, 512,           'M'],
+    'B' : [64, 64, 'M', 128, 128, 'M', 256, 256,           'M', 512, 512,           'M', 512, 512,           'M'],
+    'D' : [64, 64, 'M', 128, 128, 'M', 256, 256, 256,      'M', 512, 512, 512,      'M', 512, 512, 512,      'M'],
+    'E' : [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M']
+}
+
+class VGG(nn.Module):
+
+    def __init__(self, features, num_class=10):
+        super().__init__()
+        self.features = features
+
+        self.classifier = nn.Sequential(
+            nn.Linear(512, num_class)
+        )
+
+    def forward(self, x):
+        # output = self.features(x)
+        # output = output.view(output.size()[0], -1)
+        # output = self.classifier(output)
+        # return output
+
+        feature = self.features(x)
+        feature = feature.view(feature.size()[0], -1)
+        logits = self.classifier(feature)
+        return feature, logits
+
+def make_layers(cfg, batch_norm=False):
+    layers = []
+
+    input_channel = 3
+    for l in cfg:
+        if l == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            continue
+
+        layers += [nn.Conv2d(input_channel, l, kernel_size=3, padding=1)]#, bias=False)]
+
+        if batch_norm:
+            layers += [nn.BatchNorm2d(l)]
+        
+        layers += [nn.ReLU(inplace=True)]
+        input_channel = l
+    
+    return nn.Sequential(*layers)
+
+def vgg11_bn():
+    return VGG(make_layers(cfg['A'], batch_norm=True))
+
+def vgg13_bn():
+    return VGG(make_layers(cfg['B'], batch_norm=True))
+
+def vgg16_bn():
+    return VGG(make_layers(cfg['D'], batch_norm=True))
+
+def vgg19_bn():
+    return VGG(make_layers(cfg['E'], batch_norm=True))
